985eb0673d63fbc2d50393e3ff65fa2de54b8886
Ivykalegi <ivykalegi@gmail.com>
2022-10-07 20:43:26 +0100

    Consolidate data endpoints into a single endpoint

--- app.py
+++ app.py
@@ -3,25 +3,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+pokemon_creatures = {
+    "bulbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole",
+}
+
+
 @app.get("/")
 def pokemon_list():
-    return "Charmander, pikachu , eveee, bulbasaur, diglett"
+    return "Bulbasaur, charmander, pikachu, eevee, diglett"
 
-@app.get("/bulbasaur")
-def bulbasaur_data():
-    return "This is bulbasaur, a generation 1 pokemon who looks like a little dinosaur"
 
-@app.get("/charmander")
-def charmander_data():
-    return "This is charmander, a generation 1 pokemon who looks like a little dinosaur"
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
 
-@app.get("/eveee")
-def eveee_data():
-    return "This is eveee, a generation 1 pokemon who looks like a little rodent"
-
-@app.get("/diglett")
-def diglett_data():
-    return "This is diglett, a generation 1 pokemon who looks like a little rodent"
 
 if __name__ == "__main__":
     app.run()
